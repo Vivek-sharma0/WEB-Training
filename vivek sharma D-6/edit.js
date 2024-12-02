edb9b06e335b3c496567975b386eaa1663b4511c
Vivek-sharma0 <sharmavivek8196@gmail.com>
2024-12-02 15:48:58 +0530

    Add files via upload

---
+++ vivek sharma D-6/edit.js
@@ -0,0 +1,32 @@
+//console.log("----Welcome In Js--------");
+//
+////variables: var, let, const:
+//
+//document.write ("Hello Worls-2")
+//
+//let a = 5;
+//console.log(a)
+//var b = 5;
+//console.log(b)
+//const c = 10;
+//console.log(c)
+//a = 9;
+//b=5;
+//c=9;
+//a="abc";
+//console.log(a);
+//if(a=== 5){
+//    console.log("Hello World Im If Condition");
+//} else {
+//    console.log("else condition");
+//}
+// 
+//loops: for and while:
+
+//for (let i = 1; i <= 10; i++) {
+//   console.log(i*2);
+//}
+
+//for (let i = 1; i <= 10; i++) {
+//    console.log(i*9);
+//}
