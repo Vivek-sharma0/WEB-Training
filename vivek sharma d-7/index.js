d18fde54caabb89f0126f6c0ea8bb6b56030482d
Vivek-sharma0 <sharmavivek8196@gmail.com>
2024-12-03 15:47:27 +0530

    Add files via upload

---
+++ vivek sharma d-7/index.js
@@ -0,0 +1,24 @@
+console.log("-----Welcome In Js-----");
+const title = document.getElementById("title");
+console.log(title);
+
+title.innerText="Hlo I'm Vivek";
+
+const desc = document.getElementsByClassName("desc");
+console.log(desc);
+desc[0].innerText="Yes I'm a developer";
+
+const heading = document.getElementsByClassName("heading");
+heading[0].innerText = "start";
+heading[1].innerText = "1";
+heading[2].innerText = "2";
+heading[3].innerText = "3";
+heading[4].innerText = "4";
+heading[5].innerText = "5";
+heading[6].innerText = "6";
+heading[7].innerText = "7";
+heading[8].innerText = "8";
+heading[9].innerText = "9";
+
+console.log(heading);
+
